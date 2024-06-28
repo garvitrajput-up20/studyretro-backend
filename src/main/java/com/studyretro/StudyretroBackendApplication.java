53e29fffa9dd181f0250a452121dd97cddb3e342
garvitrajput-up20 <garvitrajput223@gmail.com>
2024-06-28 17:23:15 +0530

    FirstCommit

---
+++ src/main/java/com/studyretro/StudyretroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.studyretro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyretroBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyretroBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studyretro/StudyretroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyretro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyretroBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
